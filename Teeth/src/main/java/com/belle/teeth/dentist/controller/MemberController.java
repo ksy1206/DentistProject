dc1147688c929db9e8b4be92c666d44b8c4754e3
ksy126 <ksy.clien@gmail.com>
2018-01-26 15:27:29 +0900

    no message

--- Teeth/src/main/java/com/belle/teeth/dentist/controller/MemberController.java
+++ Teeth/src/main/java/com/belle/teeth/dentist/controller/MemberController.java
@@ -4,8 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,8 +23,19 @@ public class MemberController {
 	}
 	
 	@RequestMapping(value = "/detail", method = RequestMethod.GET)
-	public String MemberDetails(HttpServletRequest request, HttpServletResponse response) {
-		log.warn("환자 정보 페이지");
+	public String MemberDetails(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(value="type", required=false, defaultValue="info") String type, Model model) {
+		log.warn("환자 정보 페이지 type에 따라 분류");
+		
+		model.addAttribute("type", type);
+		return "dentist/detail";
+	}
+	
+	@RequestMapping(value = "/detail/qa/details", method = RequestMethod.GET)
+	public String MemberDetails(HttpServletRequest request, HttpServletResponse response, Model model) {
+		log.warn("환자 정보 페이지 type에 따라 분류");
+		
+		model.addAttribute("type", "qaDetails");
 		return "dentist/detail";
 	}
 	
