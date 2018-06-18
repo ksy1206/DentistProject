9fbbabc21bbb403df577b36e27823538a0d6debe
ksy126 <ksy.clien@gmail.com>
2018-06-18 14:59:38 +0900

    no message

--- Teeth/src/main/resources/static/js/teeth/Storage.js
+++ Teeth/src/main/resources/static/js/teeth/Storage.js
@@ -5,12 +5,13 @@ function Storage() {
 	// 아이디 저장 여부 확인
 	_this.checkLocalId = function() {
 		var localSaveID = localStorage.getItem("SaveID");
-		if(localSaveID != null || localSavdID != "") {
+
+		if(localSaveID != null) {
 			$('#userId').val(localSaveID);
 			$('#password').focus();
 		}
 	};
-	
+
 	// 로컬스토리지 아이디 저장
 	_this.saveLocalId = function(userId) {
 		localStorage.setItem("SaveID", userId);
